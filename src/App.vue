16d305ae6f74175e8fb375dcdcc435944cf594eb
Xaviera-19 <xaviera900119@gmail.com>
2023-11-26 00:58:08 +0800

    QQ

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from "vue";
+const contact = ref([
+  {
+    src: "github",
+    content: "Xaviera-19",
+  },
+  {
+    src: "mail",
+    content: "Xaviera.19.ct@gmail.com",
+  },
+  {
+    src: "github",
+    content: "Xaviera-19",
+  },
+])
 </script>
 
 <template>
   <header class="flex justify-between">
     <div class="flex gap-2 items-center py-2">
       <div class="p-1">
-        <img alt="Vue logo" class="logo w-20 h-20 " src="@/assets/xaviera_logo.jpg"/>
+        <img alt="Vue logo" class="logo h-[60px] w-[60px]" src="@/assets/xaviera_logo.jpg"/>
       </div>
       <p class="text-3xl font-bold text-[#6576f4]">Xaviera's 個人介紹網站</p>
     </div>
@@ -21,15 +36,28 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <footer class="w-full h-[100px] flex flex-col gap-2 justify-center items-center text-white">
+    <p class="text-base">Contact</p>
+    <div class="flex gap-3">
+      <div class="flex gap-1 text-sm items-center">
+        <img src="@/assets/github.svg" class="w-5 h-5">
+        <p>Xaviera-19</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+header {
+  height: 90px;
+}
   nav {
   width: 100%;
   text-align: center;
 }
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: white;
+  animation: navAnimation 1s ease-in-out infinite alternate;;
 }
 nav a.router-link-exact-active:hover {
   background-color: transparent;
@@ -37,6 +65,15 @@ nav a.router-link-exact-active:hover {
 nav a {
   display: inline-block;
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  border-left: 1px solid gray;
 }
+@keyframes navAnimation{
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+}
+
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,38 +1,64 @@
 <template>
-    <!-- foreach -->
-    <div v-for="item in data" :key="item.title" class="mb-4 px-3 py-2 flex flex-col gap-3 border border-yellow-200 border-opacity-50 rounded">
-        <div class="p-2">
-            <img class="h-[250px] rounded-ss-xl rounded-ee-xl" alt="圖片尚未出現" :src="item.src">
-        </div>
-        <!-- 文字介紹區塊 -->
-        <div class="flex flex-col grow px-2">
-            <div class="flex gap-2 items-center">
-                <div class="title text-xl font-bold">{{ item.title }}</div>
-                <a :href="item.href" class="font-bold text-base">{{ item.link }}</a>
-            </div>
-            <div class="content" v-html="item.content"></div>
-        </div>
-    </div>
-    <!-- foreach -->
+	<!-- foreach -->
+	<div
+		v-for="item in data"
+		:key="item.title"
+		class="mb-4 px-3 py-2 flex flex-col gap-3 border border-yellow-200 border-opacity-50 rounded">
+		<div class="p-2">
+			<a :href="item.href" class="image-container ">
+				<img
+					class="h-[250px] rounded-ss-xl rounded-ee-xl"
+					alt="圖片尚未出現"
+					:src="item.src"/>
+				<div class="overlay rounded-ss-xl rounded-ee-xl text-center leading-[250px] text-lg text-white font-bold">點擊前往{{ item.link }}</div>
+			</a>
+		</div>
+		<!-- 文字介紹區塊 -->
+		<div class="flex flex-col grow px-2 text-white">
+            <div class="title text-xl font-bold mb-2 underline">{{ item.title }}</div>
+			<div class="content" v-html="item.content"></div>
+		</div>
+	</div>
+	<!-- foreach -->
 </template>
 
 <script setup>
-import { ref,computed } from 'vue';
+import { ref } from "vue";
 const data = ref([
-    {
-        src : 'src/assets/exmac.png',
-        href : 'https://exmac-rebuild.dev-hub.io/',
-        title : '艾克馬 ( 專案 )',
-        link : '測試站',
-        content : '<p>此專案為全端班的Final Product，工程師確認無問題後會佈署上線。</p><p>使用Laravel10下去製作',
-    },
-    {
-        src : 'src/assets/weather_api.png',
-        href : 'https://github.com/Xaviera-19/weatherAPI',
-        title : 'Fetch Open Data Api ( 氣象卡 )',
-        link : 'GitHub',
-        content : '陣列內，天氣小卡的內容',
-    },
-
-])
+	{
+		src: "src/assets/exmac.png",
+		href: "https://exmac-rebuild.dev-hub.io/",
+		title: "艾克馬 ( 專案 )",
+		link: "測試站",
+		content:
+			"<p>此專案為全端班的Final Product，工程師確認無問題後會佈署上線。</p><p>使用Laravel10製作",
+	},
+	{
+		src: "src/assets/weather_api.png",
+		href: "https://github.com/Xaviera-19/weatherAPI",
+		title: "Fetch Open Data Api ( 氣象卡 )",
+		link: "GitHub",
+		content: "陣列內，天氣小卡的內容",
+	},
+]);
 </script>
+<style scoped>
+.image-container {
+	position: relative;
+}
+
+.overlay {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.7); /* 半透明的黑色背景 */
+	opacity: 0;
+	transition: opacity 0.3s ease;
+}
+
+.image-container:hover .overlay {
+	opacity: 1;
+}
+</style>
